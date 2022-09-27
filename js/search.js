0bc48976ec2caf84693bde9823e8a4ec5b7a0d9d
Mdshahbazalam6 <shahbazalam2002620@gmail.com>
2022-09-27 20:58:00 +0530

    assignment completed

---
+++ js/search.js
@@ -0,0 +1,58 @@
+let x;
+const do_it=()=>{
+    if(x){
+        clearTimeout(x)
+    }
+    x=setTimeout("get()",1000)
+}
+async function get(){
+    let name=document.getElementById("inp").value
+    // console.log(name)
+    let res= await fetch(`https://api.openbrewerydb.org/breweries?by_name=${name}`)
+    let data=await res.json()
+    console.log(data)
+    display(data)
+}
+
+ const  display=(ele)=>{
+  let box=document.getElementById("box")
+  box.innerHTML=``
+  ele.forEach((data)=> {
+      let div=document.createElement('div')
+    let type=document.createElement("p")
+  type.innerHTML=`Type : ${data.brewery_type} `
+
+  let city=document.createElement("p")
+   city.innerHTML=` City : ${data.city} `
+
+   let country=document.createElement("p")
+   country.innerHTML=` Country : ${data.country} `
+
+   let name=document.createElement("p")
+   name.innerHTML=` Name : ${data.name} `
+
+   let phone=document.createElement("p")
+   phone.innerHTML=` Phone : ${data.phone} `
+
+   let state=document.createElement("p")
+   state.innerHTML=` State : ${data.state} `
+
+   let website_url=document.createElement("p")
+   website_url.innerHTML=` Website_url : ${data.website_url} `
+   let a=document.createElement("a")
+   a.href="individual.html"
+   let more=document.createElement("p")
+       more.innerHTML=`More Details`
+
+       more.onclick=()=>{
+           let id=localStorage.setItem("berweryid",JSON.stringify(data.id))
+           console.log(data.id)
+       }
+       a.append(more)
+
+       more.setAttribute("class","more")
+   div.append(type,city,country,name,phone,state,website_url,a)
+   box.append(div)
+  });
+//   box.setAttribute("class","box")
+}
